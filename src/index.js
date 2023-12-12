3e8ebcc3e5df09c9725fb479c1688aeba277d2a6
abdallahelgez <kk916114@gmail.com>
2023-12-12 02:02:05 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import '@fortawesome/fontawesome-free/js/all.min.js';
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
